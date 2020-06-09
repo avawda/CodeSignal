598189c67dbffc5a360de31498434149e9b0228c
avawda <ahmed.vawda@gmail.com>
2020-06-09 10:03:26 +0200

    Initial commit

---
+++ Arcade/A - The Journey Begins/1 - add.cs
@@ -0,0 +1,10 @@
+﻿namespace TheJourneyBegins
+{
+    public class Problem1
+    {
+        public static int Add(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TheJourneyBegins;
+
+namespace CodeSignal
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Addition");
+            Console.WriteLine(Problem1.Add(10, 20));
+        }
+    }
+}
